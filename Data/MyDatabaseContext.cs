0ab70a7603cd351c478e21356031e2704daa7191
Gilbert Parga <gilbert.parga@lw.com>
2020-05-22 17:09:32 -0700

    connect to SQLDB in Azure

---
+++ Data/MyDatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoAppAzure.Data
+{
+    public class MyDatabaseContext : DbContext
+    {
+        public MyDatabaseContext(DbContextOptions<MyDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Models.Todo> Todo { get; set; }
+    }
+}
